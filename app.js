e39138594730c7fcd8e32463547b6296141b983d
Rachad-4 <rachadneyland@gmail.com>
2024-01-21 18:37:06 -0600

    Add button for selection and pokemon theme
    User selects button instead of typing choice

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
 let choices = ["Rock", "Paper", "Scissors"];
-let playerSelection = prompt("Paper, Scissors, or Rock");
-let computerSelection = getComputerChoice();
+let playerSelection = "";
+let computerSelection = "";
+let playerScore = 0;
+let cpuScore = 0;
 
+const rockBtn = document.querySelector("#Rock");
+const paperBtn = document.querySelector("#Paper");
+const scissorsBtn = document.querySelector("#Scissors");
+const roundSec = document.querySelector("#round");
+
+rockBtn.addEventListener(`click`, () => {
+    roundSec.textContent = playRound(`Rock`, getComputerChoice());
+});
+
+paperBtn.addEventListener(`click`, () => {
+    roundSec.textContent = playRound(`Paper`, getComputerChoice());
+});
+
+scissorsBtn.addEventListener(`click`, () => {
+    roundSec.textContent = playRound(`Scissors`, getComputerChoice());
+});
 
 function getComputerChoice () {
     return choices[Math.floor(Math.random() * 3)]; 
 }
 
 function playRound (playerSelection, computerSelection) {
-    let playerChoice = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase();
 
-    if (playerChoice == "Rock") {
+    if (playerSelection == "Rock") {
         if (computerSelection == "Scissors") {
-            return `You Won! ${playerChoice} beats ${computerSelection}`
+            return `${playerSelection} crushes ${computerSelection}.... Player: ${++playerScore}    Computer: 0`;
         } else if (computerSelection == "Paper") {
-            return `You Lose! ${computerSelection} beats ${playerChoice}`
+            return `You Lose! ${computerSelection} covers ${playerSelection}`
         } else return "It is tie!"
-    } else if (playerChoice == "Scissors") {
+    } else if (playerSelection == "Scissors") {
         if (computerSelection == "Paper") {
-            return `You Won! ${playerChoice} beats ${computerSelection}`
+            return `You Won! ${playerSelection} beats ${computerSelection}`
         } else if (computerSelection == "Rock") {
-            return `You Lose! ${computerSelection} beats ${playerChoice}`
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
         } else return "It is tie!"
-    } else if (playerChoice == "Paper") {
+    } else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
-            return `You Won! ${playerChoice} beats ${computerSelection}`
+            return `You Won! ${playerSelection} beats ${computerSelection}`
         } else if (computerSelection == "Scissors") {
-            return `You Lose! ${computerSelection} beats ${playerChoice}`
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
         } else return "It is tie!"    
     }
 }
 
 function game() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round ${i}`);
-        console.log(playRound());
-    }
+    console.log(`Round ${i}! Fight!!!`);
+    playerSelection = prompt("Paper, Scissors, or Rock")
+    console.log(playRound(playerSelection, computerSelection));
 }
 
-console.log("Hello, world");
-game();
+function checkScore (playerScore, computerScore) {
+    if (playerScore == 3) {
+        return `Game over! You win!!`;
+    } else if (computerScore == 3) {
+        return `Game over! You lose!!`;
+    }
+}
